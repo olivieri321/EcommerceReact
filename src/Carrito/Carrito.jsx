efc5baf9413425df28830ddfe499b89c828f03c3
olivieri321 <83927968+olivieri321@users.noreply.github.com>
2025-10-20 17:29:41 -0300

    Add files via upload

---
+++ src/Carrito/Carrito.jsx
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react';
+import { CarritoContext } from './CarritoContext';
+import styles from './Carrito.module.css'
+function Carrito() {
+ const { carrito, vaciarCarrito, eliminarDelCarrito } = useContext(CarritoContext);
+ const total = carrito.reduce((tot, producto) => tot + producto.price, 0);
+ return (
+    <>
+        <div className={styles.carrito}>
+            <h2>Carrito</h2>
+            {carrito.length > 0 ? (
+                <ul className={styles.seccionCarrito}>
+                    {carrito.map((producto, index) => (
+                            <li key={index} className={styles.elementoCarrito}>
+                                <img src={producto.image}/>
+                                <h2>{producto.title} - ${producto.price}</h2>
+                                {carrito.length > 0 && <button onClick={() => eliminarDelCarrito(index)}>Eliminar</button>}
+                            </li>
+                        
+                    ))}
+                    <div className={styles.seccionTotal}>
+                        <h1>Total: ${total.toFixed(2)}</h1>
+                        <button onClick={() => vaciarCarrito()} className={styles.botonVaciarCarrito}>Vaciar Carrito</button>
+                    </div>
+                </ul>
+                
+            ) : (
+                <p>El carrito está vacío.</p>
+            )}
+            
+            
+        </div>
+        
+    </>
+    );
+}
+export default Carrito;
